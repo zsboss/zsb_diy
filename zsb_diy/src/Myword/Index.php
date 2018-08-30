8840aa6d70471eb266fa672b850bee9780cd3a60
ZhangShiBiao <807098383@qq.com>
2018-08-30 14:25:03 +0800

    fisrt

---
+++ zsb_diy/src/Myword/Index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: boss
+ * Date: 2018/8/30
+ * Time: 下午1:05
+ */
+
+namespace Myword;
+
+
+class Index
+{
+    public function __construct()
+    {
+         echo 'Hello word,我的第一个自定义composer包';
+    }
+}
